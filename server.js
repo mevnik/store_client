00adfc33e7380f60857c7ea9bf55232d2f4a9d03
mevnik <mevnik@gmail.com>
2022-09-26 17:20:12 +0200

    26-09-22

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import path from 'path';
+import {fileURLToPath} from 'url';
 //import 'dotenv/config';
 
 
 //const PORT = process.env.PORT||8080;
 const PORT = 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
 
 const app = express();
 app.use(express.static(__dirname))
